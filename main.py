060a97f6088d2a9ee4cce7af5385b1e4e9704350
SwarupVishwas18 <swarupvishwas000@gmail.com>
2022-09-13 13:06:17 +0530

    Exception Handling

--- main.py
+++ main.py
@@ -2,15 +2,22 @@
 
 import normal, funcs
 
-while True:
-    normal.printBrand("Convert To Speech")
-    ch = normal.myMenu(["Convert Whole Text File","Convert Your Own Text","About Me","Exit"])
 
-    if ch == 1:
-        funcs.convertFileToAudio()
-    elif ch == 2:
-        funcs.convertUserInput()
-    elif ch == 3:
-        normal.aboutMe()
-    elif ch == 4:
-        normal.quitMe()
+def main():
+    while True:
+        normal.printBrand("Convert To Speech")
+        ch = normal.myMenu(["Convert Whole Text File","Convert Your Own Text","About Me","Exit"])
+
+        if ch == 1:
+            funcs.convertFileToAudio()
+        elif ch == 2:
+            funcs.convertUserInput()
+        elif ch == 3:
+            normal.aboutMe()
+        elif ch == 4:
+            normal.quitMe()
+
+try:
+    main()
+except:
+    normal.quitMe()
